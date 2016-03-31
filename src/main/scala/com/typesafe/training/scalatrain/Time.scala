65016c5703c394e875637a9c22e1c90b75901542
2ooom <dmitry.parfenchik@gmail.com>
2016-03-31 16:08:08 +0200

    Case clases

--- src/main/scala/com/typesafe/training/scalatrain/Time.scala
+++ src/main/scala/com/typesafe/training/scalatrain/Time.scala
@@ -7,20 +7,18 @@ object Time {
   val MINUTES_IN_HOUR: Int = 60
 
   def fromMinutes(minutes:Int):Time = {
-    new Time(minutes / 60, minutes% MINUTES_IN_HOUR)
+    Time(minutes / 60, minutes% MINUTES_IN_HOUR)
   }
 
   def fromHours(hours:Int):Time = ???
 }
-class Time(val hours: Int = 0,
-           val minutes: Int = 0
-          ) {
+case class Time(hours: Int = 0, minutes: Int = 0) {
   def this() = {
     this(0, 0)
   }
   require(hours >= 0 && hours < 24, "Hours must be within range of 0..23")
   require(minutes >= 0 && minutes < 60, "Minutes must be within range of 0..59")
-  val asMinutes = hours * 60 + minutes
+  val asMinutes = hours * Time.MINUTES_IN_HOUR + minutes
 
   def minus(that: Time) = {
     this.asMinutes - that.asMinutes
@@ -29,5 +27,5 @@ class Time(val hours: Int = 0,
   def -(that: Time) = minus(that)
 
   // for - new com.typesafe.training.scalatrain.Time(1, 14) => -1 - 14
-  def unary_- = new Time(-hours, minutes)
+  def unary_- = Time(-hours, minutes)
 }
--- src/main/scala/com/typesafe/training/scalatrain/Train.scala
+++ src/main/scala/com/typesafe/training/scalatrain/Train.scala
@@ -3,7 +3,7 @@ package com.typesafe.training.scalatrain
 /**
   * Created by d.parfenchik on 31/03/2016.
   */
-class Train (val number:Int, val kind:String) {
+case class Train (val number:Int, val kind:String) {
   println(s"Choo-choo: $number [$kind]")
   println(s"test ${
     val a = 10
