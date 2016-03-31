78166ff4d0e0a48ea8410fc0d9cdc9f66546093f
2ooom <dmitry.parfenchik@gmail.com>
2016-03-31 15:04:59 +0200

    Adding entry point

---
+++ src/main/scala/com/typesafe/training/scalatrain/Main.scala
@@ -0,0 +1,10 @@
+package com.typesafe.training.scalatrain
+
+/**
+  * Created by d.parfenchik on 31/03/2016.
+  */
+object Main {
+  def main(s: Array[String]):Unit = {
+    println("Hello")
+  }
+}
